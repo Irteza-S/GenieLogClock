981300a2efbf73eaca99c3ceafb3462d0fc9df31
SwiiZe <irteza.sheikh24@gmail.com>
2018-11-30 15:27:52 +0100

    ok

--- src/com/polytech/bsm/Main.java
+++ src/com/polytech/bsm/Main.java
@@ -2,12 +2,14 @@ package com.polytech.bsm;
 
 import com.polytech.bsm.clock.Clock;
 import com.polytech.bsm.observer.DefaultView;
+import com.polytech.bsm.strategy.StrategyPc;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Clock clock = new Clock();
+	public static void main(String[] args)
+	{
+		StrategyPc strat = new StrategyPc();
+		Clock clock = new Clock(strat);
 		DefaultView defView = new DefaultView();
 		
 		clock.attachObserver(defView);
--- src/com/polytech/bsm/clock/Clock.java
+++ src/com/polytech/bsm/clock/Clock.java
@@ -1,21 +1,27 @@
 package com.polytech.bsm.clock;
 
 import com.polytech.bsm.observer.Observer;
+import com.polytech.bsm.strategy.Strategy;
 import com.polytech.bsm.subject.Subject;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Clock implements Subject
+public class Clock implements Subject, Runnable
 {
 
     private LocalTime time;
     private HashMap<String, Integer> timeMap = new HashMap<>();
     private ArrayList<Observer> observerList = new ArrayList<>();
+    private Strategy strategy;
 
-    public Clock() {
+    public Clock(Strategy strategy)
+    {
     	this.time = LocalTime.now();
+    	this.strategy = strategy;
+        timeMap = new HashMap<>();
+        observerList = new ArrayList<>();
     }
 
     public void attachObserver(Observer observer)
@@ -39,10 +45,14 @@ public class Clock implements Subject
 
     private void updateTimeMap()
     {
+        time = strategy.getTime();
         timeMap.put("hours", time.getHour());
         timeMap.put("minutes", time.getMinute());
         timeMap.put("seconds", time.getSecond());
     }
 
+    public void run()
+    {
 
+    }
 }
