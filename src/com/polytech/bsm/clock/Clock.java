6a99ca78f866bb7876b4b908efc4be007282e230
boop <boop@Machine>
2018-11-30 17:14:12 +0100

    Clock recommented

--- src/com/polytech/bsm/clock/Clock.java
+++ src/com/polytech/bsm/clock/Clock.java
@@ -8,53 +8,56 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Clock implements Subject, Runnable
-{
+public class Clock implements Subject, Runnable {
 
+	// Attributes
     private LocalTime time;
     private HashMap<String, Integer> timeMap = new HashMap<>();
     private ArrayList<Observer> observerList = new ArrayList<>();
     private Strategy strategy;
 
-    public Clock(Strategy strategy)
-    {
+    // Constructors
+    public Clock(Strategy strategy) {
     	this.strategy = strategy;
     	this.time = strategy.getTime();
         timeMap = new HashMap<>();
         observerList = new ArrayList<>();
     }
 
-    public void attachObserver(Observer observer)
-    {
+    // Managing observers
+    public void attachObserver(Observer observer) {
         observerList.add(observer);
     }
-    public void detachObserver(Observer observer)
-    {
+    
+    public void detachObserver(Observer observer) {
         observerList.remove(observer);
     }
-    public void notifyObservers()
-    {
+    
+    public void notifyObservers()  {
+    	// To be sure to send the right time values
         updateTimeMap();
 
-        for(int i=0; i<observerList.size(); i++)
-        {
-            observerList.get(i).update(timeMap);
+        for(Observer obs : observerList)  {
+            obs.update(timeMap);
         }
-
     }
 
-    private void updateTimeMap()
-    {
+    // Updating time with the appropriate strategy
+    private void updateTimeMap() {
         time = strategy.getTime();
         timeMap.put("hours", time.getHour());
         timeMap.put("minutes", time.getMinute());
         timeMap.put("seconds", time.getSecond());
     }
 
-    public void run()
-    {
+    // What the thread will do
+    public void run() {
+    	
     	while(true) {
-    		notifyObservers(); 
+    		
+    		notifyObservers();
+    		
+    		// Wait a second before notifying again
     		try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
